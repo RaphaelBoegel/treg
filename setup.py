f9d37175a528805c9891a027c0f6c6a79a772565
Raphael Bögel <raphael.boegel@gmail.com>
2020-06-14 11:34:33 +0200

    pip

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://https://github.com/RaphaelBoegel/Tregex",
-    packages=setuptools.find_packages(),
+    py_modules=["treg"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
